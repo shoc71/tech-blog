c0096462f3d6e18029b9ebdda18945cfddc31e7c
shoc71 <supersonicwind6@gmail.com>
2025-12-26 02:52:25 -0500

    internet blog update

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,5 @@
 export const postData = [
-  { name: "Common Commands", date: "2025-10-15", tags: ["Reference", "INCOMPLETE"] },
-  { name: "Tabbing Obsession", date: "2025-10-22", tags: ["WCAG", "INCOMPLETE"] },
-  { name: "Social Status", date: "2025-11-11", tags: ["New", "INCOMPLETE"] },
+  { name: "Common Commands on Computers", date: "2025-10-15", tags: ["Reference", "INCOMPLETE"] },
+  { name: "Tabbing Obsession", date: "2025-12-10", tags: ["WCAG", "INCOMPLETE"] },
+  { name: "Unwritten Rules of Internet", date: "2025-12-26", tags: ["New", "INCOMPLETE"] },
 ];
