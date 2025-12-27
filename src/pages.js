0ac2fe5c200fc38bc013154ec7865854fe1ede29
shoc71 <supersonicwind6@gmail.com>
2025-12-27 00:30:49 -0500

    minor corrections

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,5 @@
 export const postData = [
   { name: "Common Commands on Computers", date: "2025-10-15", tags: ["Reference", "INCOMPLETE"] },
   { name: "Tabbing Obsession", date: "2025-12-10", tags: ["WCAG", "INCOMPLETE"] },
-  { name: "Unwritten Rules of Internet", date: "2025-12-26", tags: ["New", "INCOMPLETE"] },
+  { name: "Unwritten Rules of the Internet", date: "2025-12-26", tags: ["New", "INCOMPLETE"] },
 ];
