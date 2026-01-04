ae04ecdb7aaea2320a2da0343061f3de533fa0f5
shoc71 <supersonicwind6@gmail.com>
2026-01-04 00:20:27 -0500

    core-html-change1

--- src/pages.js
+++ src/pages.js
@@ -1,6 +1,7 @@
-export const postData = [
+export const pagesHeaderData = [
   { name: "Common Commands on Computers", date: "2025-10-15", tags: ["Reference", "INCOMPLETE"] },
   { name: "Tabbing Obsession", date: "2025-12-10", tags: ["WCAG", "INCOMPLETE"] },
   { name: "Unwritten Rules of the Internet", date: "2025-12-26", tags: ["New", "INCOMPLETE"] },
   { name: "A.I. Problems", date: "2025-12-30", tags: ["New", "INCOMPLETE"] },
+  { name: "Frontend Dev Problems", date: "2026-01-03", tags: ["New", "Hot Take", "INCOMPLETE"] },
 ];
--- src/table.js
+++ src/table.js
@@ -1,17 +1,17 @@
 import { tagColors } from "../assets/tags/tagColor.js";
-import { postData } from "./pages.js";
+import { pagesHeaderData } from "./pages.js";
 
 const displayTableData = document.querySelector("#displayTable tbody");
 
 // Create links dynamically
 const posts = Object.fromEntries(
-  postData.map(post => [
+  pagesHeaderData.map(post => [
     post.name.replace(/\s+/g, "_"),
     `<a href="./posts/${post.name.toLowerCase().replace(/\s+/g, "-")}.html" class="link-white-hover">${post.name}</a>`
   ])
 );
 
-const tableContents = postData.map((post, index) => ({
+const tableContents = pagesHeaderData.map((post, index) => ({
   id: index + 1,
   link: posts[post.name.replace(/\s+/g, "_")],
   date: post.date,
@@ -21,7 +21,7 @@ const tableContents = postData.map((post, index) => ({
 function renderTable() {
     displayTableData.innerHTML = tableContents.map(row => `
         <tr>
-        <td>${row.id}</td>
+        <td tabindex="0">${row.id}</td>
         <td style="">${row.link}</td>
         <td>${row.date}</td>
         <td style="max-width: 150px; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
