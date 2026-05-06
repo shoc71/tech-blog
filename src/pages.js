2b7dbf792d5f102a21df715cb7d3264489f78eb8
Unos <129419169+shoc71@users.noreply.github.com>
2026-05-06 00:05:14 -0400

    Rename post file and update pages data
    Rename posts/common-commands-on-computers.html to posts/common-computer-commands.html and apply small layout tweaks (add container on body and row on wrapper div). Update src/pages.js to match the new post name ("Common Computer Commands") and comment out several other page entries to temporarily disable them.

--- src/pages.js
+++ src/pages.js
@@ -1,7 +1,7 @@
 export const pagesHeaderData = [
-  { name: "Common Commands on Computers", date: "2025-10-15", tags: ["Reference", "INCOMPLETE"] },
-  { name: "Tabbing Obsession", date: "2025-12-10", tags: ["WCAG", "INCOMPLETE"] },
-  { name: "Unwritten Rules of the Internet", date: "2025-12-26", tags: ["New", "INCOMPLETE"] },
-  { name: "A.I. Problems", date: "2025-12-30", tags: ["New", "INCOMPLETE"] },
-  { name: "Frontend Dev Problems", date: "2026-01-03", tags: ["New", "Hot Take", "INCOMPLETE"] },
+  { name: "Common Computer Commands", date: "2025-10-15", tags: ["Reference", "INCOMPLETE"] },
+  // { name: "Tabbing Obsession", date: "2025-12-10", tags: ["WCAG", "INCOMPLETE"] },
+  // { name: "Unwritten Rules of the Internet", date: "2025-12-26", tags: ["New", "INCOMPLETE"] },
+  // { name: "A.I. Problems", date: "2025-12-30", tags: ["New", "INCOMPLETE"] },
+  // { name: "Frontend Dev Problems", date: "2026-01-03", tags: ["New", "Hot Take", "INCOMPLETE"] },
 ];
